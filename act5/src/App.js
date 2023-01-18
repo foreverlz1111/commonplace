2cd5cde3aa8d3eee3455a3b1344e6de5863519a7
foreverlz1111 <hi@ziyuan.work>
2023-01-18 17:08:08 +0800

    更新文件夹

---
+++ act5/src/App.js
@@ -0,0 +1,116 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+
+                <Form/>
+            </header>
+        </div>
+    );
+}
+
+function Form() {
+    const [person, setPerson] = useState({
+        name: 'Niki de Saint Phalle',
+        artwork: {
+            title: 'Blue Nana',
+            city: 'Hamburg',
+            image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+        }
+    });
+
+    function handleNameChange(e) {
+        setPerson({
+            ...person,
+            name: e.target.value
+        });
+    }
+
+    function handleTitleChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                title: e.target.value
+            }
+        });
+    }
+
+    function handleCityChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                city: e.target.value
+            }
+        });
+    }
+
+    function handleImageChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                image: e.target.value
+            }
+        });
+    }
+
+    return (
+        <>
+            <label>
+                Name:
+                <input
+                    value={person.name}
+                    onChange={handleNameChange}
+                />
+            </label>
+            <label>
+                Title:
+                <input
+                    value={person.artwork.title}
+                    onChange={handleTitleChange}
+                />
+            </label>
+            <label>
+                City:
+                <input
+                    value={person.artwork.city}
+                    onChange={handleCityChange}
+                />
+            </label>
+            <label>
+                Image:
+                <input
+                    value={person.artwork.image}
+                    onChange={handleImageChange}
+                />
+            </label>
+            <p>
+                <i>{person.artwork.title}</i>
+                {' by '}
+                {person.name}
+                <br/>
+                (located in {person.artwork.city})
+            </p>
+            <img
+                src={person.artwork.image}
+                alt={person.artwork.title}
+            />
+        </>
+    );
+}
+
+export default App;
