6fb5a5841994d41c97c1cbbdaefba9c672cfe629
foreverlz1111 <hi@ziyuan.work>
2023-01-26 22:44:47 +0800

    更新文件夹 Focusing a text input

--- act8/src/App.js
+++ act8/src/App.js
@@ -1,35 +1,44 @@
-
 import './App.css';
 import {useRef} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (
+        <div className="App">
+            <header className="App-header">
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <Clicker/>
-      </header>
-    </div>
-  );
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+
+                <Clicker/>
+            </header>
+        </div>
+    );
 }
-function Clicker(){
+
+function Clicker() {
     let ref = useRef(0)
-    function handleClick(){
+
+    function handleClick() {
         ref.current = ref.current + 1
     }
-    return(
+
+    function handleClickFocus() {
+        ref.current.focus()
+    }
+
+    return (
         <>
             <p>(Won't ReRender)ref:{ref.current}</p>
-            <button onClick={handleClick}><b>+</b></button>
+            <input ref={ref}/>
+            <button onClick={handleClickFocus}><b>+</b></button>
         </>
     )
 }
+
 export default App;
