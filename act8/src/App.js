5be99be81aeb86409a50c53965a10a143affed4b
foreverlz1111 <hi@ziyuan.work>
2023-01-26 23:13:09 +0800

    更新文件夹 Scrolling to an element

--- act8/src/App.js
+++ act8/src/App.js
@@ -16,6 +16,7 @@ function App() {
                 </a>
 
                 <Clicker/>
+                <CatScroll/>
             </header>
         </div>
     );
@@ -37,6 +38,60 @@ function Clicker() {
             <p>(Won't ReRender)ref:{ref.current}</p>
             <input ref={ref}/>
             <button onClick={handleClickFocus}><b>+</b></button>
+        </>
+    )
+}
+
+
+function CatScroll() {
+    const firstref = useRef(null)
+    const secondref = useRef(null)
+    const thirdref = useRef(null)
+
+    const Cats = [
+        {src: "https://placekitten.com/g/200/200", alt: "Tom", ref: firstref},
+        {src: "https://placekitten.com/g/300/200", alt: "Maru", ref: secondref},
+        {src: "https://placekitten.com/g/250/200", alt: "Jellylorum", ref: thirdref},
+    ]
+
+    function handleScrollToFirst() {
+        firstref.current.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "center"
+        })
+    }
+
+    function handleScrollToSecond() {
+        secondref.current.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "center"
+        })
+    }
+
+    function handleScrollToThird() {
+        thirdref.current.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+            inline: "center"
+        })
+    }
+
+    return (
+        <>
+            <nav>
+                <button onClick={handleScrollToFirst}>1</button>
+                <button onClick={handleScrollToSecond}>2</button>
+                <button onClick={handleScrollToThird}>3</button>
+            </nav>
+            <div>
+                <ul>
+                    {Cats.map(c => (
+                        <li><img src={c.src} alt={c.alt} ref={c.ref}/></li>
+                    ))}
+                </ul>
+            </div>
         </>
     )
 }
