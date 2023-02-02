43a42760e1007c513fe87cdf1cfc04ac9464cce6
foreverlz1111 <hi@ziyuan.work>
2023-02-02 17:28:55 +0800

    更新文件夹 add Welcome logo

--- act10/src/App.js
+++ act10/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {showNotification} from "./notifications";
 import {createConnection} from "./chat";
+import "./welcome.css"
 
 function App() {
     const [position, setPosition] = useState({x: 0, y: 0})
@@ -31,6 +32,7 @@ function App() {
                         Learn React
                     </a>
 
+                    <Welcome/>
                     <Movable canMove={canMove}
                              setCanMove={setCanMove}/>
                     <MyChatRoom/>
@@ -49,8 +51,48 @@ function App() {
                 height: 20
             }}/>
         </>
-
     );
+}
+
+function Welcome() {
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const duration = 1000
+        const node = ref.current
+
+        let startTime = performance.now()
+        let frameId = null
+
+        function onFrame(now) {
+            const timePassed = now - startTime
+            const progress = Math.min(timePassed / duration, 1)
+            onProgress(progress)
+            if (progress < 1) {
+                frameId = requestAnimationFrame(onFrame)
+            }
+        }
+
+        function onProgress(progress) {
+            node.style.opacity = progress
+        }
+
+        function start() {
+            onProgress(0)
+            startTime = performance.now()
+            frameId = requestAnimationFrame(onFrame)
+        }
+
+        function stop() {
+            cancelAnimationFrame(frameId)
+            startTime = null
+            frameId = null
+        }
+
+        start()
+        return () => stop()
+    }, [])
+    return (<h1 className="welcome" ref={ref}>Welcome</h1>)
 }
 
 const serverUrl = "https://localhost:1234"
